0444b1595c9c9c3f4d508abe768554ec0067914c
bili <jambon_bilitis@hotmail.com>
2022-07-08 13:09:34 +0200

    modifs 8/7

--- connexion.js
+++ connexion.js
@@ -1,25 +1,42 @@
-const urlApi = 'http://localhost:8888/api_php/';
-const connectForm = document.querySelector('.connexion');
+const urlApi = 'http://localhost:8888/api_php/'
+const connectForm = document.querySelector('.connexion')
+const deconnectLink = document.querySelector('.deconnexion')
 
-connectForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let ident = {
-        login: document.querySelector('.login').value ,
-        password: document.querySelector('.password').value 
-    }
-    console.log(ident)
-    fetch(urlApi + 'auth', {
-        method: 'POST',
-        headers: {
-            'content-Type':'application/json'
-        },
-        body: JSON.stringify(ident)
-    })
-    .then(response => {
-        response.json()
-    })
-    .then (response => {
-        console.log(response)
-    })
-    .catch(error => console.error(error))
-})
+        deconnectLink.addEventListener('click', (e) => {
+            e.preventDefault()
+            fetch(urlApi + 'deconnect')
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => console.error(error))
+        })
+
+
+        connectForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            let ident = {
+                login: document.querySelector('.login').value ,
+                password: document.querySelector('.password').value
+            }
+
+            fetch(urlApi + 'auth', {
+                headers: {
+                    "content-type": "application/json"
+                },
+                method: 'POST',
+                //mode:"no-cors",
+                body: JSON.stringify(
+                    ident
+                )
+            })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                // récupération du token
+                if(response.code == 200) {
+                    localStorage.setItem('token', response.token)
+                }
+            })
+            .catch(error => console.log(error))
+        })
